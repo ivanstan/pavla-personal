09b03153e2ed1f031e624ffc224773a47e616a5e
Ivan Stanojevic <ivanstan@gmail.com>
2017-10-03 19:31:06 +0200

    loader added

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+    $(document).ready(function () {
+        setTimeout(function(){
+            $('.loading').remove();
+        }, 1000);
+    });
+
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
